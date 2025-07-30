511a83f39f6855c94e1f5982c43ada9ae57a9c46
AdithyaMurugan <164379628+AdithyaMurugan@users.noreply.github.com>
2025-07-30 22:40:42 +0530

    Add files via upload
    Adding Week 6 Additional Hands on

---
+++ Week6-Additional-Handson/6. ReactJS-HOL/trainersapp/src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to My Academy Trainers App</h2>
+      <p>This app helps track trainer details with routing features.</p>
+    </div>
+  );
+};
+
+export default Home;
